b371976defe5796060fc1407520091135624cf3b
cmattes <corey-mattes@uiowa.edu>
2014-02-13 16:54:29 -0600

    Test
    This is a test to see if this works.

---
+++ SonicJump/Assets/Scripts/Scroll.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroll : MonoBehaviour {
+
+	public float speed = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		renderer.material.mainTextureOffset = new Vector2 (0f,Time.time * speed);
+	}
+
+	//	void OnGUI(){
+	//		GUI.Box (Rect.MinMaxRect(Camera.main.transform.position.x,Camera.main.transform.position.y,100,100), Camera.main.fieldOfView.ToString());
+	//	}
+}
