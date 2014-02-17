e44c33be71a1d368373c934021280a1b12b581d0
Maarij <mohammadmaarij-anwar@uiowa.edu>
2014-02-17 10:17:36 -0600

    updated scrolling to only move when w is pressed

--- SonicJump/Assets/Scripts/Scroll.cs
+++ SonicJump/Assets/Scripts/Scroll.cs
@@ -3,7 +3,10 @@ using System.Collections;
 
 public class Scroll : MonoBehaviour {
 
-	public float speed = 0;
+	public float speed = 0.2f;
+	public float new_y = 0f;
+	public float old_y = 0f;
+	public float current_y = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -12,7 +15,16 @@ public class Scroll : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		renderer.material.mainTextureOffset = new Vector2 (0f,Time.time * speed);
+		//renderer.material.mainTextureOffset = new Vector2 (0f,Time.time * speed);
+		//Debug.Log ((renderer.material.GetTextureOffset("_MainTex")));
+
+		if (Input.GetKey(KeyCode.W)){
+			Debug.Log (Time.time);
+			//old_y = old_y + 0.1;
+			new_y = Time.time * speed;
+			renderer.material.mainTextureOffset = new Vector2 (0f,old_y); new Vector2(0f, new_y);
+			old_y = new_y;
+		}
 	}
 
 	//	void OnGUI(){
