4dd4f10e04e4f7deeff467f4595f43d9a7489a23
Maarij <mohammadmaarij-anwar@uiowa.edu>
2014-02-17 11:14:08 -0600

    BG now follows character left and right

--- SonicJump/Assets/Scripts/Scroll.cs
+++ SonicJump/Assets/Scripts/Scroll.cs
@@ -2,33 +2,49 @@
 using System.Collections;
 
 public class Scroll : MonoBehaviour {
-
 	public float speed = 0.2f;
 	public float new_y = 0f;
-	public float old_y = 0f;
+	public float new_x = 0f;
 	public float current_y = 0f;
+	public float current_x = 0f;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKey(KeyCode.D)){
+			Debug.Log (current_x);
+
+			new_x = new_x + 0.02f;
+			current_x = new_x * speed;
+			renderer.material.mainTextureOffset = new Vector2(current_x, current_y);
+		}
+
+		if (Input.GetKey(KeyCode.A)){
+			Debug.Log (current_x);
+			
+			new_x = new_x - 0.02f;
+			current_x = new_x * speed;
+			renderer.material.mainTextureOffset = new Vector2(current_x, current_y);
+		}
+
 		if (Input.GetKey(KeyCode.W)){
 			//Debug.Log (Time.time);
 			//old_y = old_y + 0.1;
 			new_y = new_y + 0.02f;
-			renderer.material.mainTextureOffset = new Vector2(0f, new_y * speed);
-			//old_y = new_y;
+			current_y = new_y * speed;
+			renderer.material.mainTextureOffset = new Vector2(current_x, current_y);
 		}
 
 		if (Input.GetKey(KeyCode.S)){
 			//Debug.Log (Time.time);
-			//old_y = old_y + 0.1;
+
 			new_y = new_y - 0.02f;
-			renderer.material.mainTextureOffset = new Vector2(0f, new_y * speed);
-			//old_y = new_y;
+			current_y = new_y * speed;
+			renderer.material.mainTextureOffset = new Vector2(current_x, current_y);
 		}
 	}
 
