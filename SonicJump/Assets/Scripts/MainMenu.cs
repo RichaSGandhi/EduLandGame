8a6e742b285b6a72858286d2e9d610f57e8ccd86
Maarij <mohammadmaarij-anwar@uiowa.edu>
2014-02-23 20:43:05 -0600

    main menu

---
+++ SonicJump/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	void OnGUI(){
+		if (GUI.Button (new Rect (0, 0, 50, 25), "Menu")) 
+		{
+			GUI.Button (new Rect(100,100,300,300), "asdf");
+			Debug.Log("Box should have been created");
+		}
+	}
+}
