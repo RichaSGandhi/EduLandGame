1266d960ff3d44b617599c50a6723a2a48d77b9b
Maarij <mohammadmaarij-anwar@uiowa.edu>
2014-03-26 23:17:56 -0500

    Renewed Project

---
+++ EdulandTheGame/Assets/Scripts/credits.cs
@@ -0,0 +1,16 @@
+﻿//C#
+using UnityEngine;
+using System.Collections;
+
+public class credits : MonoBehaviour {
+	void OnGUI () {
+		// Make a background box
+		GUI.contentColor = Color.black;
+		GUI.Label(new Rect(Screen.width/2 - 100, Screen.height/2 - 40, 100, 90), "Developers:");
+		GUI.Label(new Rect(Screen.width/2, Screen.height/2 - 40, 100, 90), "Patrick Walsh");
+		GUI.Label(new Rect(Screen.width/2, Screen.height/2 - 20, 100, 90), "Shivani Metes");
+		GUI.Label(new Rect(Screen.width/2, Screen.height/2, 100, 90), "Corey Mattes");
+		GUI.Label(new Rect(Screen.width/2, Screen.height/2 + 20, 100, 90), "Richa Ghandi");
+		GUI.Label(new Rect(Screen.width/2, Screen.height/2 + 40, 100, 90), "Maarij Anwar");
+	}
+}
