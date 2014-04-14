12c3b91e517f4240495866f5683a7e6f804f5932
RichaSGandhi <richa-gandhi@uiowa.edu>
2014-04-14 12:33:19 -0500

    adding test case

---
+++ EdulandTheGame/Assets/Scripts/Editor/PlayerMoveTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+
+
+internal class PlayerMoveTest{
+
+	[Test]
+	public void obstacleDestroyed() 
+
+	{
+			if (PlayerMove.destroyedPrevious == true) {
+			Assert.Pass();
+			Debug.Log ("Previous Object Destroyed");
+		}
+		
+	}
+}
