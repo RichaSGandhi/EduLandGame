f54c5c41e978358b15de7cd1943428570dc1cb15
RichaSGandhi <richa-gandhi@uiowa.edu>
2014-05-04 03:41:01 -0500

    Adding save game and highcore database

--- EdulandTheGame/Assets/Scripts/PlayerMove.cs
+++ EdulandTheGame/Assets/Scripts/PlayerMove.cs
@@ -23,7 +23,17 @@ public class testPlayer
 		vect.Set(xValue,yVlaue);
 		return vect;
 	}
-	
+
+	public Vector2 setVelocity2(float move, float rB2DYVelocity)
+	{
+		//Debug.Log ("Yayy..!! Setting new velocity from separate class");
+		Vector2 vect= new Vector2();
+		float xValue = move * 6f;
+		float yVlaue = rB2DYVelocity;
+		vect.Set(xValue,yVlaue);
+		return vect;
+	}
+
 	public bool Flip (bool faceRight){
 		bool fR;
 		fR = !faceRight; 
@@ -36,6 +46,8 @@ public class testPlayer
 		RaycastHit2D whatIsHit = Physics2D.Linecast (lineStart, lineEnd, 1<< LayerMask.NameToLayer("Enemies"));
 		//return whatIsHit;
 		if (whatIsHit) {
+			//Debug.Log ("Line start value" + lineStart);
+			//Debug.Log ("Line end value" + lineEnd);
 			myInteract = true;
 		} else {
 			myInteract = false;
@@ -68,11 +80,13 @@ public class testPlayer
 	public bool checkStringEqual(string textBoxString, GameObject gameObj)
 	{
 		bool isEqual = false;
-		char[] endClone = {'C','l','o','n','e','(',')'};
-		string trimmedString = gameObj.name.TrimEnd (endClone);
+		//char[] endClone = {'(','C','l','o','n','e',')'};
+		string trimmedString = gameObj.name.Remove((gameObj.name.Length - 7),7);
+		//Debug.Log(trimmedString);
+		//textBoxString = textBoxString.ToUpper();
+		trimmedString = trimmedString.ToUpperInvariant();
 		if (textBoxString == trimmedString)
 			isEqual = true;
-		
 		return isEqual;
 	}
 	
@@ -84,7 +98,9 @@ public class PlayerMove : MonoBehaviour {
 	public float maxSpeed = 7f;
 	bool facingRight= true;
 	string scoreText;
-	private int score;
+	string highScoreText;
+	public static int score;
+	int highScore;
 	public GUIStyle labelStyle;
 	Animator Anim;
 	public bool interact = false;
@@ -103,13 +119,20 @@ public class PlayerMove : MonoBehaviour {
 	public AudioSource[] sounds;
 	public AudioSource noise1;
 	public AudioSource noise2;
-
+	public bool L5 = false;
+	private bool skipped = false;
 	bool doubleJump = false;
 	public static bool destroyedPrevious= false;
 	public static float timer;
+	public GUIStyle cutSceneStyle;
+	public bool cs01 = false;
+	public bool cs12 = false;
+	public bool isCutScene = false;
+
 
 	void Start ()
 	{
+		highScore = PlayerPrefs.GetInt(Application.loadedLevelName + "HighScore");
 		Anim = GetComponent<Animator> ();
 		if(GameObject.Find ("Spawner")){
 			GameObject spawner = GameObject.Find ("Spawner");
@@ -117,6 +140,7 @@ public class PlayerMove : MonoBehaviour {
 			anotherScript.spawnRandomObject (-1);
 		}
 		scoreText = "Score : " + score.ToString();
+		highScoreText = "High Score : " + highScore.ToString();
 		sounds = GetComponents<AudioSource>();
 		noise1 = sounds[0];
 		noise2 = sounds[1];
@@ -125,39 +149,96 @@ public class PlayerMove : MonoBehaviour {
 	void FixedUpdate ()
 	{
 				//myFixedUpdate ();
-		
-				grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
-				Anim.SetBool ("Ground", grounded);
-		
-				if (grounded) {
+		if (isCutScene == true) {
+			grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+			Anim.SetBool ("Ground", grounded);
+			
+			if (grounded) {
+				doubleJump = false;
+			}
+			
+			
+			Anim.SetFloat ("vSpeed", rigidbody2D.velocity.y);
+			
+			float move = Input.GetAxis ("Horizontal");
+			
+			Anim.SetFloat ("Speed", Mathf.Abs (move));
+			
+			/*if (move > 0 && transform.position.x < 10) {
+				if (!grounded && L5) {
+					rigidbody2D.velocity = tp.setVelocity2 (move, rigidbody2D.velocity.y);// new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+				} else {
+					rigidbody2D.velocity = tp.setVelocity (move, rigidbody2D.velocity.y);
+				}
+			} else if (move < 0 && transform.position.x <= 8.5f) {	
+				if (!grounded && L5) {
+					rigidbody2D.velocity = tp.setVelocity2 (move, rigidbody2D.velocity.y);// new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+				} else {*/
+			rigidbody2D.velocity = tp.setVelocity (move, rigidbody2D.velocity.y);
+			//}
+			//}
+			
+			if(transform.position.x > 10 && cs01 == true){
+				Application.LoadLevel ("Level1");
+			}
+			else if(transform.position.x > 10 && cs12 == true){
+				Application.LoadLevel ("Level2");
+			}
+			
+			if (move > 0 && !facingRight) {
+				facingRight = tp.Flip (facingRight);
+				Vector3 theScale = transform.localScale;
+				theScale.x *= -1;
+				transform.localScale = theScale;				
+			} else if (move < 0 && facingRight) {
+				facingRight = tp.Flip (facingRight);
+				Vector3 theScale = transform.localScale;
+				theScale.x *= -1;
+				transform.localScale = theScale;
+			}
+		}
+				else{
+					grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+					Anim.SetBool ("Ground", grounded);
+			
+					if (grounded) {
 						doubleJump = false;
-				}
-		
-		
-				Anim.SetFloat ("vSpeed", rigidbody2D.velocity.y);
-		
-				float move = Input.GetAxis ("Horizontal");
-		
-				Anim.SetFloat ("Speed", Mathf.Abs (move));
-		
-				if (move > 0 && transform.position.x < 4) {
-						rigidbody2D.velocity = tp.setVelocity (move, rigidbody2D.velocity.y);// new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
-				} else if (move < 0 && transform.position.x <= 8.5f) {	
-						rigidbody2D.velocity = tp.setVelocity (move, rigidbody2D.velocity.y);
-				}
-		
-		
+					}
+			
+			
+					Anim.SetFloat ("vSpeed", rigidbody2D.velocity.y);
+			
+					float move = Input.GetAxis ("Horizontal");
+			
+					Anim.SetFloat ("Speed", Mathf.Abs (move));
+			
+					if (move > 0 && transform.position.x < 4) {
+						if (!grounded && L5) {
+							rigidbody2D.velocity = tp.setVelocity2 (move, rigidbody2D.velocity.y);// new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+						} else {
+							rigidbody2D.velocity = tp.setVelocity (move, rigidbody2D.velocity.y);
+						}
+					} else if (move < 0 && transform.position.x <= 8.5f) {	
+						if (!grounded && L5) {
+							rigidbody2D.velocity = tp.setVelocity2 (move, rigidbody2D.velocity.y);// new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+						} else {
+							rigidbody2D.velocity = tp.setVelocity (move, rigidbody2D.velocity.y);
+						}
+					}
+			
+			
 				if (move > 0 && !facingRight) {
-						facingRight = tp.Flip (facingRight);
-						Vector3 theScale = transform.localScale;
-						theScale.x *= -1;
-						transform.localScale = theScale;				
+					facingRight = tp.Flip (facingRight);
+					Vector3 theScale = transform.localScale;
+					theScale.x *= -1;
+					transform.localScale = theScale;				
 				} else if (move < 0 && facingRight) {
-						facingRight = tp.Flip (facingRight);
-						Vector3 theScale = transform.localScale;
-						theScale.x *= -1;
-						transform.localScale = theScale;
+					facingRight = tp.Flip (facingRight);
+					Vector3 theScale = transform.localScale;
+					theScale.x *= -1;
+					transform.localScale = theScale;
 				}
+			}
 		}
 
 	void Raycast(){
@@ -179,14 +260,6 @@ public class PlayerMove : MonoBehaviour {
 			if(!doubleJump && !grounded)
 				doubleJump = true;
 			rigidbody2D.AddForce(tp.setRigidBodyForce(doubleJump, grounded, jumpForce));
-			/*if(!doubleJump && !grounded){
-				doubleJump = true;
-				rigidbody2D.AddForce(new Vector2(0, jumpForce/2));
-				audio.PlayOneShot(clip);
-			}
-			else{
-				rigidbody2D.AddForce(new Vector2(0, jumpForce));
-*/
 			noise1.Play ();
 		}
 		interact = tp.checkObsctacle (lineStart.position, lineEnd.position);
@@ -199,9 +272,13 @@ public class PlayerMove : MonoBehaviour {
 			destroyedPrevious = tp.destroyObstacle(whatIHit.collider.gameObject);
 			//Destroy(whatIHit.collider.gameObject);
 			//destroyedPrevious = true;
-			score = score + 1;
+			if (skipped == false)
+				score = score + 20;
+			else 
+				score = score - 5; 
 			scoreText = "Score : " + score.ToString();
-			
+			Debug.Log ("In start" + score);
+			//highScoreText = "High Score : " + highScore.ToString();
 			SpawnMultipleObject.startTime = Time.time;
 			stringToEdit = "";
 			noise2.Play ();
@@ -216,26 +293,51 @@ public class PlayerMove : MonoBehaviour {
 		transform.localScale = theScale;
 	}*/
 	void OnGUI () {
-		GUI.Label(new Rect (1200, 25, 30, 30), scoreText, labelStyle);
-		
-		
-		if (interact) {
-			GUI.color = Color.white;
-			stringToEdit = GUI.TextField (new Rect (1000, 120, 200, 50), stringToEdit, 25);
-			if(whatIHit)
-			{
-				//Debug.Log(whatIHit.collider.gameObject.name.TrimEnd(endClone));
-				//string objectName = ;
-				isCollision = tp.checkStringEqual(stringToEdit, whatIHit.collider.gameObject);
-				if(isCollision)
-					stringToDisplay ="Cool, Hit 'Enter' and Go ahead!";
-				else 
-					stringToDisplay = "Hey!! What's my name?";
+		if (isCutScene == true) {
+						if (cs01 == true) {
+								GUI.contentColor = Color.black;
 				
-				GUI.Label(new Rect (1000, 170, 200, 60), stringToDisplay);
+								GUI.Label (new Rect (500, 50, 700, 500), "Our story begins with a young girl named Kairi waking up to find that her entire town is empty aside from one oddity: a fairy floating in the middle of town.  \n\nKairi: \"Where has everyone gone? Who are you? WHAT are you? \n\n\"Fairy: \"You seek answers? Head to the plains of Language Arts in the East. Perhaps there are some for you there.\"", cutSceneStyle);
+						} else if (cs12 == true) {
+								GUI.contentColor = Color.black;
 				
-			}
-		}
+								GUI.Label (new Rect (500, 50, 700, 500), "Kairi:\"I haven't seen so many creatures on the plains in a while, something must be wrong.\"\n\nFairy:\"Correct! Something is indeed wrong. I will answer one of your questions from home: I am a Fairy! Please proceed further east to the Forest of Mathematics to receive more answers.\"\n\nKairi: \"Mythological creatures are appearing now? I better investigate these woods.\"", cutSceneStyle);
+				
+						}
+				} else {
+						
+						GUI.Label (new Rect (1100, 25, 30, 30), scoreText, labelStyle);
+				
+						GUI.Label (new Rect (1250, 25, 30, 30), highScoreText, labelStyle);
+						if (interact) {
+								if (Application.loadedLevelName == "Level1"){
+								GUI.contentColor = Color.black;
+								}
+								GUI.color = Color.white;
+								stringToEdit = GUI.TextField (new Rect (900, 120, 350, 50), stringToEdit, 25);
+								//char[] endClone = {'C','l','o','n','e','(',')'};
+								if (whatIHit) {
+										//Debug.Log(whatIHit.collider.gameObject.name.TrimEnd(endClone));
+										//string objectName = ;
+										isCollision = tp.checkStringEqual (stringToEdit, whatIHit.collider.gameObject);
+										if (isCollision) {
+												stringToDisplay = "Correct Answer! You got 20 Points. Hit 'Enter' and Go Ahead.";
+												skipped = false;
+										} else {
+												stringToEdit = stringToEdit.ToUpperInvariant ();
+												if (stringToEdit == "SKIP") {
+														stringToDisplay = "Sorry! You lost 5 points. Hit 'Enter' and Go Ahead.";
+														isCollision = true;	
+														skipped = true;
+												} else
+														stringToDisplay = "Hey!! Guess me OR Enter 'SKIP' to move ahead";
+										}
+				
+										GUI.Label (new Rect (910, 170, 370, 60), stringToDisplay);
+
+								}
+						}
+				}
 }
 }
 
